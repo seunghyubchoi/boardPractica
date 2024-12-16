40a80d6f44290a715653329bd06e06d813b2b336
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-12-16 21:50:57 +0900

    componentScan

--- shBoardWeb/src/main/java/polymorphism/LgTV.java
+++ shBoardWeb/src/main/java/polymorphism/LgTV.java
@@ -1,5 +1,8 @@
 package polymorphism;
 
+import org.springframework.stereotype.Component;
+
+@Component("tv")
 public class LgTV implements TV{
 	public LgTV() {
 		System.out.println("====> LgTV °´Ã¼ »ý¼º");
