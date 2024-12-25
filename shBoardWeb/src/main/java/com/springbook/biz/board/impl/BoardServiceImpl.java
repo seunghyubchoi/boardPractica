49a54ef4dd16d381139ebf631f041ea550544c00
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-12-25 20:17:16 +0900

    BoardServiceImpl 클래스와 LogAdvice 객체가 소스코드에서 강하게 결합

--- shBoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ shBoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.common.Log4Advice;
 import com.springbook.biz.common.LogAdvice;
 
 @Service("boardService")
@@ -14,39 +15,50 @@ public class BoardServiceImpl implements BoardService {
 
 	@Autowired
 	private BoardDAO boardDAO;
-	private LogAdvice log;
+	//private LogAdvice log;
+	private Log4Advice log;
 	
+	/*
 	public BoardServiceImpl() {
 		log = new LogAdvice();
+	}
+	*/
+	public BoardServiceImpl() {
+		log = new Log4Advice();
 	}
 	
 	@Override
 	public void insertBoard(BoardVO vo) {
-		log.printLog();
+		//log.printLog();
+		log.printLogging();
 		boardDAO.insertBoard(vo);
 	}
 
 	@Override
 	public void updateBoard(BoardVO vo) {
-		log.printLog();
+		//log.printLog();
+		log.printLogging();
 		boardDAO.updateBoard(vo);
 	}
 
 	@Override
 	public void deleteBoard(BoardVO vo) {
-		log.printLog();
+		//log.printLog();
+		log.printLogging();
 		boardDAO.deleteBoard(vo);
 	}
 
 	@Override
 	public BoardVO getBoard(BoardVO vo) {
-		log.printLog();
+		//log.printLog();
+		log.printLogging();
 		return boardDAO.getBoard(vo);
 	}
 
 	@Override
 	public List<BoardVO> getBoardList(BoardVO vo) {
-		log.printLog();
+		//log.printLog();
+		log.printLogging();
 		return boardDAO.getBoardList(vo);
 	}
 
