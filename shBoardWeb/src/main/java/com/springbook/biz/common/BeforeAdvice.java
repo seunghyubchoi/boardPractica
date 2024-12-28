81f103be828e80718a2a42147d9ae7dae43648cf
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-12-28 09:27:21 +0900

    테스트

--- shBoardWeb/src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ shBoardWeb/src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -2,7 +2,7 @@ package com.springbook.biz.common;
 
 public class BeforeAdvice {
 	public void beforeLog() {
-		System.out.println("[사전 처리] 비즈니스 로직 수행 전 동작");
+		System.out.println("[사전 처리] 비즈니스 로직 수행 전 동작 테스트");
 	}
 
 }
