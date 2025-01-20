368dd88a704e5eb8e97e7898bbd99b8c6e974792
Seunghyub Choi <seunghyub.choi@gmail.com>
2025-01-20 21:44:08 +0900

    use mdl

--- shBoardWeb/src/main/java/com/springbook/view/board/BoardController.java
+++ shBoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -1,14 +1,30 @@
 package com.springbook.view.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
 public class BoardController {
+	
+	// 검색 조건 목록 설정
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap() {
+		System.out.println("searchConditionMap 실행");
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
+		return conditionMap;
+	}
+	
 	// 글 등록
 	@RequestMapping("/insertBoard.do")
 	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
@@ -32,17 +48,15 @@ public class BoardController {
 	
 	// 글 상세 조회
 	@RequestMapping("/getBoard.do")
-	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
-		mav.addObject("board", boardDAO.getBoard(vo)); // Model 정보 저장
-		mav.setViewName("getBoard.jsp"); // View 정보 저장
-		return mav;
+	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
+		model.addAttribute("board", boardDAO.getBoard(vo)); // Model 정보 저장
+		return "getBoard.jsp";
 	}	
 
 	// 글 목록 검색
 	@RequestMapping("/getBoardList.do")
-	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
-		mav.addObject("board", boardDAO.getBoardList(vo)); // Model 정보 저장
-		mav.setViewName("getBoardList.jsp"); // View 정보 저장
-		return mav;
+	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
+		model.addAttribute("boardList", boardDAO.getBoardList(vo));
+		return "getBoardList.jsp";
 	}		
 }
