df9f46b5fffce60faf1a1f5e276c96800c8429de
Seunghyub Choi <seunghyub.choi@gmail.com>
2025-01-21 22:28:08 +0900

    set2

--- shBoardWeb/src/main/java/com/springbook/view/board/BoardController.java
+++ shBoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -2,6 +2,7 @@ package com.springbook.view.board;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
-import com.springbook.biz.board.impl.BoardDAO;
-
+@Controller
 @SessionAttributes("board")
 public class BoardController {
 	
