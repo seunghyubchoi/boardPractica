3d42ab38d703dd240ce9ebe135597e6b666e0e4b
Seunghyub Choi <seunghyub.choi@gmail.com>
2025-01-28 18:31:54 +0900

    prsnt layer mod

--- shBoardWeb/src/main/java/com/springbook/view/user/LoginController.java
+++ shBoardWeb/src/main/java/com/springbook/view/user/LoginController.java
@@ -3,7 +3,6 @@ package com.springbook.view.user;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,10 +21,13 @@ public class LoginController {
 		
 
 	@RequestMapping(value="/login.do", method=RequestMethod.POST)
-	public String login(UserVO vo, UserDAO userDAO) {
-		System.out.println("로그인 인증 처리...");
+	public String login(UserVO vo, UserDAO userDAO, HttpSession session) {
+		if(vo.getId() == null || vo.getId().equals("")) {
+			throw new IllegalArgumentException("아이디는 반드시 입력해야 합니다.");
+		}
 		UserVO user = userDAO.getUser(vo);
 		if(user != null) {
+			session.setAttribute("userName", user.getName());
 			return "getBoardList.do";
 		} else {
 			return "login.jsp";
