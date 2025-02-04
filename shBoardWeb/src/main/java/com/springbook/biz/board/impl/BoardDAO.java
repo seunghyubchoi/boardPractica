4dfa1074a749aa28472080e6c8babf1149b7f199
Seunghyub Choi <seunghyub.choi@gmail.com>
2025-02-04 21:18:15 +0900

    mod

--- shBoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ shBoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -23,7 +23,7 @@ public class BoardDAO {
 	private final String BOARD_UPDATE = "update board set title=?, content=? where seq=?";
 	private final String BOARD_DELETE = "delete board where seq=?";
 	private final String BOARD_GET = "select * from board where seq=?";
-	private final String BOARD_LIST = "select * from board order by seq desc";
+	//private final String BOARD_LIST = "select * from board order by seq desc";
 	private final String BOARD_LIST_T = "select * from board where title like '%'||?||'%' order by seq desc";
 	private final String BOARD_LIST_C = "select * from board where content like '%'||?||'%' order by seq desc";
 	
